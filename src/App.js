4cbbf93ddfc4840d802f449df1303e861abdca69
Roni <ronniiii@yahoo.com>
2023-08-28 20:33:45 +0100

    [SETUP]html markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="darkbtn">
+          <i className="fas fa-adjust"></i>
+        </div>
+        <div className="top">
+          <h1>
+            <i className="fas fa-book"></i> My <span className="green">Courses</span>{" "}
+            List App
+          </h1>
+        </div>
       </header>
-    </div>
+      <main></main>
+    </>
   );
 }
 
